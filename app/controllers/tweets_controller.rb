c188dbc1248bd4c969769eb1a4e583875eb0dfe5
ytsurumis <156957587+ytsurumis@users.noreply.github.com>
2025-11-21 16:14:20 +0900

    first commit

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,44 @@
+class TweetsController < ApplicationController
+  before_action :set_tweet, only: [:edit, :show]
+  
+  def index
+    @tweets = Tweet.all
+  end
+
+  def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    Tweet.create(tweet_params)
+    redirect_to '/'
+  end
+
+  def destroy
+    tweet = Tweet.find(params[:id])
+    tweet.destroy
+    redirect_to root_path
+  end
+
+  def edit
+  end
+
+  def update
+    tweet = Tweet.find(params[:id])
+    tweet.update(tweet_params)
+    redirect_to root_path
+  end
+
+  def show
+  end
+
+  private
+  def tweet_params
+    params.require(:tweet).permit(:name, :image, :text)
+  end
+
+  def set_tweet
+    @tweet = Tweet.find(params[:id])
+  end
+
+end
