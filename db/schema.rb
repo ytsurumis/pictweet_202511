605365e69b0de327b3992a416bf8055add8c13cd
ytsurumis <156957587+ytsurumis@users.noreply.github.com>
2025-11-26 14:40:28 +0900

    マイページの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_11_26_012417) do
+ActiveRecord::Schema[7.1].define(version: 2025_11_26_053235) do
   create_table "tweets", charset: "utf8mb3", force: :cascade do |t|
-    t.string "name"
     t.string "text"
     t.text "image"
     t.datetime "created_at", null: false
