1a80170dc8494560e6ea24d8155a2187482056ce
ytsurumis <156957587+ytsurumis@users.noreply.github.com>
2025-12-02 22:23:38 +0900

    コメント投稿フォームの追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < ApplicationController
   def create
     comment = Comment.create(comment_params)
-    redirect_to "/twwts/#{comment.tweet.id}"
+    redirect_to "/tweets/#{comment.tweet.id}"
   end
 
   private
