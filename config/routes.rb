add0ffa4c85e31d0ef8fe5106998413453e3034b
ytsurumis <156957587+ytsurumis@users.noreply.github.com>
2025-12-03 21:17:06 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'tweets#index'
   resources :tweets do
-    resources :comments, only: [:create]
+    resources :comments, only: :create
+    collection do
+      get 'search'
+    end
   end
-  resources :users, only: [:show]
+  resources :users, only: :show
 end
